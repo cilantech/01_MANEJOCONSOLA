44e2bd0a1ce71c0ab9733ad8942b42b2fdfc0b6c
cilantech <isazaherbin@gmail.com>
2024-04-15 07:42:21 -0500

    se agregan variables y se

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
-var name = "Herbin Esteban"
+var userName = "Herbin Esteban"
 var lastN = "Restrepo isaza"
 var age = "23"
 var name2 = "Herbin esteban restrepo isaza 23"
-
-alert(name, age, lastN)
-
-alert(name2)
-console.log(name, age, lastN)
+const email = "herbin3522@gmail.com"
+let address = "la floresta pa calle siempre viva"
+let callNumber = "2241646584"
+//let
+//const
+//var se usaba mucho antes ahora lo abandonamos,siempre usar: let si se puede alterar pero const no
+console.log(userName, age, lastN)
 console.log(name2)
+console.log(email,address,callNumber)

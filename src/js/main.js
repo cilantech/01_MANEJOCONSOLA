509b5d1b98f1ae341873a6111ad4ec0ca5e409e8
cilantech <isazaherbin@gmail.com>
2024-04-15 07:25:39 -0500

    Se nombraron las variables como se debe en ingles !!!A,ERICA

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+var name = "Herbin Esteban"
+var lastN = "Restrepo isaza"
+var age = "23"
+var name2 = "Herbin esteban restrepo isaza 23"
+
+alert(name, age, lastN)
+
+alert(name2)
+console.log(name, age, lastN)
+console.log(name2)
